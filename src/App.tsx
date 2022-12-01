e16611681e311007807f5a42031a29695f45ed99
Andrés Felipe Martínez Salazar <andresfelipe.2031@gmail.com>
2022-11-30 23:30:41 -0500

    sortable list with drag and drop

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, CSSProperties } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const numbers = new Array(10).fill(0).map((it, idx) => idx + 1);
+
+function arraymove<T>(arr: T[], fromIndex: number, toIndex: number): T[] {
+  const tmp = [...arr];
+  tmp.splice(fromIndex, 1);
+  tmp.splice(toIndex, 0, arr[fromIndex]);
+  return tmp;
 }
+
+const itemStyle: (isDragging: boolean) => CSSProperties = (isDragging) => ({
+  border: `1px solid ${isDragging ? "white" : "gray"}`,
+});
+
+const SortableList = () => {
+  const [items, setItems] = useState(numbers);
+  const [draggingItem, setDraggingItem] = useState<number | null>(null);
+
+  return (
+    <ul>
+      {items.map((item, idx) => (
+        <li
+          key={idx}
+          className="list-item"
+          style={itemStyle(draggingItem === item)}
+          onDragEnter={() =>
+            draggingItem !== null &&
+            setItems((prev) =>
+              arraymove(prev, prev.indexOf(draggingItem), prev.indexOf(item))
+            )
+          }
+          onDragEnd={() => setDraggingItem(null)}
+        >
+          <span className="list-item-content">{item}</span>
+          <span
+            className="drag-handle"
+            draggable
+            onDragStart={() => setDraggingItem(item)}
+          >
+            |||
+          </span>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const App = () => (
+  <div className="container-main flex">
+    <h1>Drag and drop sortable</h1>
+    <div
+      style={{
+        display: "flex",
+      }}
+    >
+      <SortableList />
+    </div>
+  </div>
+);
 
 export default App;
